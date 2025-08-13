f5cadacd2ac51399677a4fac209eaf587069b193
sachinsparsh <77607442+sachinsparsh@users.noreply.github.com>
2025-08-13 19:48:42 +0530

    Update sh15.sh

--- sh15.sh
+++ sh15.sh
@@ -2,11 +2,9 @@
 services="aws docker terraform"
 for i in $services
 do
-	serviceStatus=sudo service $i status
-	echo "service status---> $serviceStatus"
-	sudo systemctl is-active --quiet "$i"
-	if [ $? -eq 0 ]; then
-		echo "$i is running"
-	else echo "$i is stopped"
-	fi
+	if systemctl is-active --quiet "$service"; then
+    echo "$service is running"
+else
+    echo "$service is NOT running"
+fi
 done
